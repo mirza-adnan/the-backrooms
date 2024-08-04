0824c0dbe1b617a04908a9c30c0c08937e777901
Mirza Adnan <mirza.adnan2205@gmail.com>
2024-08-05 00:12:52 +0600

    add checks to see if username is already being used in a channel

--- server.js
+++ server.js
@@ -2,28 +2,99 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const { Server } = require("socket.io");
+const bcrypt = require("bcrypt");
+const { v4 } = require("uuid");
 
 // INITIALIZATION
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const channels = new Map();
+channels.set("tp", {
+  messages: [],
+  password: "armsskl2003",
+  members: [],
+});
+channels.set("IUT CSE 22", {
+  messages: [],
+  password: "iutcse22",
+  members: [],
+});
+
 // MIDDLEWARE
 app.use(express.static(__dirname + "/static"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/static/index.html"));
 });
 
-io.on("connection", (socket) => {
-  console.log("a user has connected");
+app.post("/", async (req, res) => {
+  const { username, channelName, channelPassword } = req.body;
 
-  socket.on("text", (msg) => {
-    io.emit("text", msg);
+  if (!channels.has(channelName)) {
+    return res
+      .status(404)
+      .json({ message: "A channel with that name does not exist" });
+  }
+
+  if (channelPassword != channels.get(channelName).password) {
+    return res.status(401).json({
+      message: "Password does not match the specified channel's password",
+    });
+  }
+
+  if (channels.get(channelName).members.some((member) => member === username)) {
+    return res.status(400).json({
+      message: "A person with that username is already in the channel",
+    });
+  }
+
+  return res.status(200).json({
+    messages: channels
+      .get(channelName)
+      .messages.slice(
+        Math.max(channels.get(channelName).messages.length - 20, 0)
+      ),
+    id: v4(),
+  });
+});
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} has connected`);
+
+  let username = "";
+  let channelName = "";
+
+  socket.on("login", (data) => {
+    username = data.username;
+    channelName = data.channelName;
+
+    channels.get(channelName).members.push(username);
+    console.log(channels.get(channelName).members);
+    console.log(`${username} has joined ${channelName}`);
+
+    socket.on("disconnect", () => {
+      const channel = channels.get(channelName);
+      channel.members = channel?.members?.filter(
+        (member) => member != username
+      );
+      console.log(`${username} has disconnected from ${channelName}`);
+    });
   });
 
-  socket.on("disconnect", (socket) => {
-    console.log("a user has disconnected");
+  socket.on("text", (message) => {
+    const channel = channels.get(message.channelName);
+
+    channel.messages.push(message);
+    if (channel.messages.length >= 30) {
+      channel.messages = channel.messages.slice(
+        Math.max(channel.messages.length - 20, 0)
+      );
+    }
+
+    io.emit(message.channelName, message);
   });
 });
 
--- static/script.js
+++ static/script.js
@@ -1,12 +1,126 @@
 const socket = io();
 
+const user = {
+  username: "",
+  channel: "",
+  id: "",
+};
+
+const chatScreen = document.querySelector("#chat-layout");
+const authScreen = document.querySelector("#auth-layout");
+const channelTitle = document.querySelector("#channel-name");
+
 const form = document.querySelector("#text-form");
 const input = document.querySelector("#text-input");
 const messages = document.querySelector("#messages");
 
-function createMessageComponent(msg, name) {
+function showChat() {
+  chatScreen.classList.remove("hidden");
+  chatScreen.classList.add("flex");
+
+  authScreen.classList.add("hidden");
+}
+
+(function Auth() {
+  console.log("ran auth");
+  const inputs = {
+    username: document.querySelector("#username-input"),
+    channelName: document.querySelector("#channel-name-input"),
+    channelPassword: document.querySelector("#channel-password-input"),
+  };
+  const authForm = document.querySelector("#auth-form");
+
+  const login = (e) => {
+    e.preventDefault();
+
+    const username = inputs.username.value.trim();
+    const channelName = inputs.channelName.value.trim();
+    const channelPassword = inputs.channelPassword.value.trim();
+    console.log(channelPassword);
+
+    if (username && channelName && channelPassword) {
+      const reqBody = {
+        username,
+        channelName,
+        channelPassword,
+      };
+
+      fetch("http://localhost:3131", {
+        method: "POST",
+        body: JSON.stringify(reqBody),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          if (res.status === 401) {
+            throw new Error(
+              "Password does not match the specified channel's password"
+            );
+          } else if (res.status === 404) {
+            throw new Error("Channel with that name does not exist");
+          } else if (res.status === 400) {
+            throw new Error(
+              "A member with that name is already in the channel"
+            );
+          }
+          return res.json();
+        })
+        .then((data) => {
+          user.username = username;
+          user.channelName = channelName;
+          if (localStorage.getItem(username)) {
+            user.id = localStorage.getItem(username);
+          } else {
+            localStorage.setItem(username, data.id);
+            user.id = data.id;
+          }
+
+          socket.emit("login", {
+            username: user.username,
+            channelName: user.channelName,
+          });
+
+          channelTitle.textContent = channelName;
+
+          showChat();
+
+          data.messages.forEach((message) => {
+            createMessageComponent(
+              message.content,
+              message.author.name,
+              message.author.id === user.id
+            );
+          });
+
+          socket.on(user.channelName, (message) => {
+            createMessageComponent(
+              message.content,
+              message.author.name,
+              message.author.id === user.id
+            );
+            messages.scrollTop = messages.scrollHeight;
+          });
+
+          messages.scrollTop = messages.scrollHeight;
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    } else {
+      alert("One of the fields is blank");
+    }
+  };
+
+  authForm.addEventListener("submit", login);
+})();
+
+function createMessageComponent(msg, name, isReply) {
   const container = document.createElement("div");
   container.className = "message";
+  if (isReply) {
+    container.classList.add("flex", "flex-col", "items-end");
+  }
 
   const user = document.createElement("p");
   user.className = "text-sm text-white/50 px-2 mb-1";
@@ -27,12 +141,11 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   if (input.value.trim()) {
-    socket.emit("text", input.value);
+    socket.emit("text", {
+      content: input.value,
+      author: { name: user.username, id: user.id },
+      channelName: user.channelName,
+    });
     input.value = "";
   }
-});
-
-socket.on("text", (msg) => {
-  createMessageComponent(msg, "Mirza Adnan");
-  messages.scrollTop = messages.scrollHeight;
 });
